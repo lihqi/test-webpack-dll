d9d1f243188144cdef686951e83d4f25ff1c78dd
lihqi <455711093@qq.com>
2019-01-16 21:43:30 +0800

    y

--- config/webpack.dll.config.js
+++ config/webpack.dll.config.js
@@ -14,12 +14,12 @@ module.exports = {
   output: {
     path: resolveApp("build"),
     filename: "[name].dll.js",
-    library: "_dll_[name]" // 全局变量名，其他模块会从此变量上获取里面模块
+    library: "_dll_vendor" // 全局变量名，其他模块会从此变量上获取里面模块
   },
   // manifest是描述文件
   plugins: [
     new webpack.DllPlugin({
-      name: "_dll_[name]",
+      name: "_dll_vendor",
       path: resolveApp("build/manifest.json")
     })
   ]
